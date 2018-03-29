2d3fddb1e4ea14154627e67880c1b31c5bbefcae
Evan <peterse583@gmail.com>
2018-03-29 11:24:46 -0400

    initial commit
    Going to check installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='projectinit',
+      version='0.1',
+      description='a python project to initialize python projects',
+      url='',
+      author='EVAN',
+      author_email='peterse583@gmail.com',
+      license='MIT',
+      packages=['projectinit'],
+      zip_safe=False)
